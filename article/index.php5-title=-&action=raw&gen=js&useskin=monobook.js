9ff2f18c8283ef6fe2a6092e180f5ecb2525c287
Matthieu Napoli <matthieu@mnapoli.fr>
2015-11-03 21:54:43 +0100

    First commit

---
+++ article/index.php5-title=-&action=raw&gen=js&useskin=monobook.js
@@ -0,0 +1,9 @@
+/* generated javascript */
+var skin = 'monobook';
+var stylepath = '/article/skins';
+
+/* MediaWiki:Common.js */
+/* N’importe quel JavaScript ici sera chargé pour n’importe quel utilisateur et pour chaque page accédée. */
+
+/* MediaWiki:Monobook.js (deprecated; migrate to Common.js!) */
+/* Périmé : utilisez [[MediaWiki:common.js]] */
